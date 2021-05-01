72f20d2ee17e53933a1fd889abdd38f3b3f0cf03
BJS-kr <78771384+BJS-kr@users.noreply.github.com>
2021-05-01 20:03:23 +0900

    Create 4.py

---
+++ algorithm/greedy/4.py
@@ -0,0 +1,15 @@
+# https://www.acmicpc.net/problem/10162
+target = int(input())
+btns = list([300, 60, 10])
+A,B,C=0,0,0
+
+if target%btns[2]!=0:
+    print(-1)
+else:
+    A = target//btns[0]
+    target -= A*btns[0]
+    B = target//btns[1]
+    target -= B*btns[1]
+    C = target//btns[2]
+    target -= C*btns[2]
+    print(A, B, C)
