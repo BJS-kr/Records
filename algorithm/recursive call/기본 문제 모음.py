5533f507f6f0a19ada2ae65d26dc0ee4a45a4a9d
BJS-kr <78771384+BJS-kr@users.noreply.github.com>
2021-05-08 01:54:35 +0900

    Update 기본 문제 모음.py

--- algorithm/recursive call/기본 문제 모음.py
+++ algorithm/recursive call/기본 문제 모음.py
@@ -54,3 +54,49 @@ def palin_recur(word):
         return False
 
 palin_recur('motom')
+
+# 예제 4 풀이
+def sick_cal(n):
+    print(int(n))
+    if n == 1:
+        return
+    elif n % 2 != 0:
+        n = n * 3 + 1
+        sick_cal(n)
+    else:
+        n = n / 2
+        sick_cal(n)
+
+sick_cal(11)
+
+# 예제 4 개선
+def sick_cal(n):
+    print(int(n))
+    if n == 1:
+        return
+    elif n % 2 != 0:
+        sick_cal(n * 3 + 1)
+    else:
+        sick_cal(n / 2)
+        
+# 예제 5 : 1,2,3을 조합하여 n을 이루는 모든 경우의 수를 구하라
+
+# 풀이 1: 틀림. 이건 1,2,3으로 가능한 모든 조합을 표시해주긴 하나, 
+# 조합 순서의 개념까진 가지고 있지 않음. 1+1+2와 1+2+1을 같은 것으로 처리함.
+# 거기다 재귀적으로 풀지도 않음
+def combi(n):
+    combicount = list()
+    a = n
+    b = n // 2
+    c = n // 3
+    for i in range(a+1):
+        for j in range(b+1):
+            for k in range(c+1):
+                if i + j*2 + k*3 == n:
+                    combicount.append([i,j,k])
+    return combicount
+
+combi(6)
+
+
+ 
