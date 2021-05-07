8c4e1dc946738397930d43aba48397603d896e28
BJS-kr <78771384+BJS-kr@users.noreply.github.com>
2021-05-07 20:04:28 +0900

    Create selection.py

---
+++ algorithm/sort/selection.py
@@ -0,0 +1,23 @@
+# 선택정렬 : 가장 작은 거 찾아서 앞 인덱스로 배치, 배치한 인덱스는 제외하고 나머지에서 계속 반복
+
+# 값이 두개일땐?
+def sel_sort(*args):
+    a = list(args)
+    if a[0] < a[1]:
+        pass
+    else:
+        a[0], a[1] = a[1], a[0]
+    return a
+
+# 값이 여러개 일땐?
+def sel_sort2(*args):
+    a = list(args)
+    for i in range(len(a)):
+        if i == len(a) - 1:
+            break
+        for j in range(i + 1, len(a)):
+            if a[j] < a[i]: 
+                a[j], a[i] = a[i], a[j]
+    return a
+
+sel_sort2(9,2,1,5,7,4,8,3,6)
