9f391178f3c63a6be119a21c075cc8fc96ac4fee
BJS-kr <78771384+BJS-kr@users.noreply.github.com>
2021-05-10 01:39:24 +0900

    Create merge_sort.py

---
+++ algorithm/sort/merge_sort.py
@@ -0,0 +1,28 @@
+from random import sample as sam
+
+def split_merge(list_):
+    if len(list_) <= 1:
+        return list_
+    left = split_merge([val for val in list_[:len(list_)//2]])
+    right = split_merge([val for val in list_[len(list_)//2:]])
+    return merge(left, right)
+    
+def merge(left, right):
+    sorted_list = list()
+    while left and right:
+        if min(left) > min(right):
+            sorted_list.append(min(right))
+            right.remove(min(right))
+        else:
+            sorted_list.append(min(left))
+            left.remove(min(left))
+    while left:
+        sorted_list.append(min(left))
+        left.remove(min(left))
+    while right:
+        sorted_list.append(min(right))
+        right.remove(min(right))
+    return sorted_list
+        
+    
+split_merge(sam(range(100), 10))
