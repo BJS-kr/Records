cdf9454fadc3e03b4aa0e0b07fa38b15227835d0
BJS-kr <78771384+BJS-kr@users.noreply.github.com>
2021-05-10 04:02:00 +0900

    Update sequential.py

--- algorithm/search_basic/sequential.py
+++ algorithm/search_basic/sequential.py
@@ -1,1 +1,14 @@
+# 순차적, 직선적 값 찾기
 
+from random import randint as ri
+
+ran_ints = [ri(1,100) for _ in range(10)]
+target = ri(1,100)
+
+def seq_srch(ran_ints, target):
+    for i in range(len(ran_ints)):
+        if ran_ints[i] == target:
+            return i
+    return False
+
+seq_srch(ran_ints, target)
