b0a667db9352691da16b70220b666885ecfc21e9
BJS-kr <78771384+BJS-kr@users.noreply.github.com>
2021-05-10 00:31:36 +0900

    Update quick_sort.py

--- algorithm/sort/quick_sort.py
+++ algorithm/sort/quick_sort.py
@@ -1,7 +1,7 @@
 # pivot값을 기준으로 두개의 리스트로 분리
 # 길이가 1이 될때까지 재귀적으로 반복 후 left+[pivot]+right
 
-import random
+from random import sample as sam
 
 def qsort(list_):
     if len(list_) <= 1:
@@ -11,4 +11,4 @@ def qsort(list_):
     right = qsort([val for val in list_[1:] if val >= pivot]) # pivot과 동일한 값 여러개면 누락되니까 >=
     return left + [pivot] + right # list와 int는 연산 불가능하니까 [pivot]
 
-qsort(random.sample(range(100), 10))
+qsort(sam(range(100), 10))
