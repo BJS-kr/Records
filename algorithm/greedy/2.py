7a054970b46f0684445274db5b266fd3291194f1
BJS-kr <78771384+BJS-kr@users.noreply.github.com>
2021-05-12 07:17:57 +0900

    Update 2.py

--- algorithm/greedy/2.py
+++ algorithm/greedy/2.py
@@ -1,4 +1,5 @@
 # https://www.acmicpc.net/problem/11399
+# 처음에 푼답
 customers = int(input())
 waiting_times = sorted(list(map(int, input().split())))
 total_waiting_times = 0
@@ -7,6 +8,19 @@ for i in range(len(waiting_times)):
     total_waiting_times += sum(waiting_times[:i+1])
 
 print(total_waiting_times)
+
+# 나중에 푼답
+
+cutomers = int(input())
+times = sorted(list(map(int,input().split())))
+accu = 0
+total = 0
+
+for time in times:
+    accu += time
+    total += accu
+
+print(total)
 
 # 승준's 피드백: 중간에 계속 sum을 하기 보다, 값을 기억해놓고 한개씩 더하는게 조금더 좋을듯??
 customers = int(input())
