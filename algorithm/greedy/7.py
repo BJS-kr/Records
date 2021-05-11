4e2a41110442043a54548de23fd0361760c88773
BJS-kr <78771384+BJS-kr@users.noreply.github.com>
2021-05-12 05:11:21 +0900

    Update 7.py

--- algorithm/greedy/7.py
+++ algorithm/greedy/7.py
@@ -1,5 +1,7 @@
 # https://www.acmicpc.net/problem/13305
 # 현재 위치의 가격보다 싼 가격까지 도달하는 거리까지 만큼 기름을 넣는다. 도달한 후 반복한다.
+# if 논리 배치 순서도 실행시간에 영향을 크게 미친다. if elif 위치 바꿔서 통과했음. 실행이 가장 적어지는 순서대로. 
+
 
 city_total = int(input())
 road_length = list(map(int,input().split()))
