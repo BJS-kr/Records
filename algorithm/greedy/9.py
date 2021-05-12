625991f98b43cb25637308fe05ac23777271a9db
BJS-kr <78771384+BJS-kr@users.noreply.github.com>
2021-05-13 08:32:39 +0900

    Update 9.py

--- algorithm/greedy/9.py
+++ algorithm/greedy/9.py
@@ -12,3 +12,17 @@ for coin in coins:
             break
             
 print(result)
+
+# 개선
+target = 1000 - int(input())
+coins = [500, 100, 50, 10, 5, 1]
+result = 0
+
+for coin in coins:
+    if coin <= target:
+        result += target//coin
+        target %= coin
+        if target == 0:
+            break
+            
+print(result)
