1ea9eb35a811b84e253c4a9f1d5f44c7e2718e75
BJS-kr <78771384+BJS-kr@users.noreply.github.com>
2021-05-12 20:42:08 +0900

    Update 8.py

--- algorithm/greedy/8.py
+++ algorithm/greedy/8.py
@@ -1,5 +1,6 @@
-https://www.acmicpc.net/problem/11047
+# https://www.acmicpc.net/problem/11047
 
+# sys쓰면 실행시간 줄어든다해서 사용했는데 왜 더 길게 나옴?(76ms)
 import sys
 
 n,k = map(int,input().split())
@@ -14,4 +15,19 @@ for value in values:
         if k == 0:
             break
             
+print(result)
+
+# sys 안 쓴거(72ms)
+n,k = map(int,input().split())
+values = sorted([int(input()) for _ in range(n)],reverse=True)
+result = 0
+
+for value in values:
+    if value <= k:
+        n = k//value
+        result += n
+        k -= n*value
+        if k == 0:
+            break
+
 print(result)
