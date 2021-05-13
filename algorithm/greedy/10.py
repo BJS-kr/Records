937012e215d7126de4c7097ac76072543dd0cc6d
BJS-kr <78771384+BJS-kr@users.noreply.github.com>
2021-05-14 00:08:04 +0900

    Update 10.py

--- algorithm/greedy/10.py
+++ algorithm/greedy/10.py
@@ -1,5 +1,6 @@
 #https://www.acmicpc.net/problem/1541
 a = input().split('-')
+b = [int(v) for v in ''.join(a[1:]).split('+')]
 result = 0
 
 # a[0]은 무조건 양수
@@ -9,14 +10,15 @@ if '+' in a[0]:
         result += int(i)
 else:
     result = int(a[0])
+    
+result -= sum(b)
+print(result)
 
-# 이 후의 모든 값은 무조건 -이므로 처리
-for i in range(1,len(a)):
-    if '+' in a[i]:
-        b = a[i].split('+')
-        for j in b:
-            result -= int(j)
-    else:
-        result -= int(a[i])
+# 스터디원의 도움을 받아 개선
+a = [list(map(int,v.split('+'))) for v in input().split('-')]
+result = sum(a[0])
 
+for i in range(len(a[1:])):
+    result -= sum(a[1:][i])
+    
 print(result)
