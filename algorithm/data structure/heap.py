c087ef1831a1cd97a9a105021afe341aa6ca70a3
BJS-kr <78771384+BJS-kr@users.noreply.github.com>
2021-05-14 16:31:48 +0900

    Create heap.py

---
+++ algorithm/data structure/heap.py
@@ -0,0 +1,92 @@
+# 가이드를 보고 그대로 구현해보았다
+
+class Heap:
+    def __init__(self, data):
+        self.heap_array = list()
+        self.heap_array.append(None) # index0을 None으로 둬서 array를 1부터 사용해서 직관성 높이기
+        self.heap_array.append(data)
+        
+    def move_down(self, popped_idx):
+        left_child_popped_idx = popped_idx * 2
+        right_child_popped_idx = popped_idx * 2 + 1
+        
+        # case1 : 왼쪽 자식 없을 때. 왼쪽이 없다면 오른쪽도 없음.
+        if left_child_popped_idx >= len(self.heap_array):
+            return False
+        #case2 : 오른쪽만 없을때
+        elif right_child_popped_idx >= len(self.heap_array):
+            if self.heap_array[popped_idx] < self.heap_array[left_child_popped_idx]:
+                return True
+            else:
+                return False
+        #case3 : 왼쪽 오른쪽 모두 존재
+        else:
+            if self.heap_array[left_child_popped_idx] > self.heap_array[right_child_popped_idx]:
+                if self.heap_array[popped_idx] < self.heap_array[left_child_popped_idx]:
+                    return True
+                else:
+                    return False
+            else:
+                if self.heap_array[popped_idx] < self.heap_array[right_child_popped_idx]:
+                    return True
+                else:
+                    return False
+    
+    def pop(self):
+        if len(self.heap_array) <= 1:
+            return None
+        
+        returned_data = self.heap_array[1]
+        self.heap_array[1] = self.heap_array[-1]
+        del self.heap_array[-1]
+        popped_idx = 1
+        
+        while self.move_down(popped_idx):
+            left_child_poppped_idx = popped_idx * 2
+            right_child_popped_idx = popped_idx * 2 + 1
+            
+            # case2 : 오른쪽 자식 노드만 없을 때
+            if right_child_popped_idx >= len(self.heap_array):
+                if self.heap_array[popped_idx] < self.heap_array[left_child_popped_idx]:
+                    self.heap_array[popped_idx], self.heap_array[left_child_popped_idx] = self.heap_array[left_child_popped_idx], self.heap_array[popped_idx]
+                    popped_idx = left_child_popped_idx
+            # case3 : 왼쪽 오른쪽 모두 있을 때
+            else:
+                if self.heap_array[left_child_popped_idx] > self.heap_array[right_child_popped_idx]:
+                    if self.heap_array[popped_idx] < self.heap_array[left_child_popped_idx]:
+                        self.heap_array[popped_idx], self.heap_array[left_child_popped_idx] = self.heap_array[left_child_popped_idx], self.heap_array[popped_idx]
+                        popped_idx = left_child_popped_idx
+                else:
+                    if self.heap_array[popped_idx] < self.heap_array[right_child_popped_idx]:
+                        self.heap_array[popped_idx], self.heap_array[right_child_popped_idx] = self.heap_array[right_child_popped_idx], self.heap_array[popped_idx]
+                        popped_idx = right_child_popped_idx
+                    
+        return returned_data
+
+             
+    def move_up(self, inserted_idx):
+        if inserted_idx <= 1:
+            return False
+        
+        parent_idx = inserted_idx // 2
+        if self.heap_array[inserted_idx] > self.heap_array[parent_idx]:
+            return True
+        else:
+            return False
+    
+    def insert(self, data):
+        if len(self.heap_array) == 0: # init에서 data가 안들어왔을 경우 init역할 시행
+            self.heap_array.append(None)
+            self.heap_array.append(data)
+            return True
+        
+        self.heap_array.append(data) # insert의 본 역할
+        
+        inserted_idx  = len(self.heap_array) - 1
+        
+        while self.move_up(inserted_idx):
+            parent_idx = inserted_idx // 2
+            self.heap_array[inserted_idx], self.heap_array[parent_idx] = self.heap_array[parent_idx], self.heap_array[inserted_idx]
+            inserted_idx = parent_idx
+            
+        return True
