db4859e98b96709686eebcb20cca2c4a21637950
BJS-kr <78771384+BJS-kr@users.noreply.github.com>
2021-06-16 20:03:47 +0900

    Create deque methods.py

---
+++ 내 맘대로 파이썬 공부/deque methods.py
@@ -0,0 +1,8 @@
+# deque.append(item): item을 데크의 오른쪽 끝에 삽입한다.
+# deque.appendleft(item): item을 데크의 왼쪽 끝에 삽입한다.
+# deque.pop(): 데크의 오른쪽 끝 엘리먼트를 가져오는 동시에 데크에서 삭제한다.
+# deque.popleft(): 데크의 왼쪽 끝 엘리먼트를 가져오는 동시에 데크에서 삭제한다.
+# deque.extend(array): 주어진 배열(array)을 순환하면서 데크의 오른쪽에 추가한다.
+# deque.extendleft(array): 주어진 배열(array)을 순환하면서 데크의 왼쪽에 추가한다.
+# deque.remove(item): item을 데크에서 찾아 삭제한다.
+# deque.rotate(num): 데크를 num만큼 회전한다(양수면 오른쪽, 음수면 왼쪽).
