f54c0fecdf7764f9ddaee82a1f315a600b791e7c
BJS-kr <78771384+BJS-kr@users.noreply.github.com>
2022-02-19 22:16:20 +0900

    Create abstract_factory.ts

---
+++ design_patterns/abstract_factory.ts
@@ -0,0 +1,88 @@
+interface AbstractFactory {
+    createProductA(): AbstractProductA;
+
+    createProductB(): AbstractProductB;
+}
+
+class ConcreteFactory1 implements AbstractFactory {
+    public createProductA(): AbstractProductA {
+        return new ConcreteProductA1();
+    }
+
+    public createProductB(): AbstractProductB {
+        return new ConcreteProductB1();
+    }
+}
+
+class ConcreteFactory2 implements AbstractFactory {
+    public createProductA(): AbstractProductA {
+        return new ConcreteProductA2();
+    }
+
+    public createProductB(): AbstractProductB {
+        return new ConcreteProductB2();
+    }
+}
+
+interface AbstractProductA {
+    usefulFunctionA(): string;
+}
+
+class ConcreteProductA1 implements AbstractProductA {
+    public usefulFunctionA(): string {
+        return 'The result of the product A1.';
+    }
+}
+
+class ConcreteProductA2 implements AbstractProductA {
+    public usefulFunctionA(): string {
+        return 'The result of the product A2.';
+    }
+}
+
+interface AbstractProductB {
+    usefulFunctionB(): string;
+    anotherUsefulFunctionB(collaborator: AbstractProductA): string;
+}
+
+class ConcreteProductB1 implements AbstractProductB {
+
+    public usefulFunctionB(): string {
+        return 'The result of the product B1.';
+    }
+
+    public anotherUsefulFunctionB(collaborator: AbstractProductA): string {
+        const result = collaborator.usefulFunctionA();
+        return `The result of the B1 collaborating with the (${result})`;
+    }
+}
+
+class ConcreteProductB2 implements AbstractProductB {
+
+    public usefulFunctionB(): string {
+        return 'The result of the product B2.';
+    }
+
+    public anotherUsefulFunctionB(collaborator: AbstractProductA): string {
+        const result = collaborator.usefulFunctionA();
+        return `The result of the B2 collaborating with the (${result})`;
+    }
+}
+
+
+function clientCode(factory: AbstractFactory) {
+    const productA = factory.createProductA();
+    const productB = factory.createProductB();
+
+    console.log(productB.usefulFunctionB());
+    console.log(productB.anotherUsefulFunctionB(productA));
+}
+
+
+console.log('Client: Testing client code with the first factory type...');
+clientCode(new ConcreteFactory1());
+
+console.log('');
+
+console.log('Client: Testing the same client code with the second factory type...');
+clientCode(new ConcreteFactory2());
