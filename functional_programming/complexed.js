6fd56e94ec0cef89c4d27c6c4bf9403b385d3b84
BJS-kr <78771384+BJS-kr@users.noreply.github.com>
2022-02-20 18:51:29 +0900

    Create complexed.js

---
+++ functional_programming/complexed.js
@@ -0,0 +1,60 @@
+const { log, error } = console;
+
+const target = [
+  1,
+  2,
+  Promise.resolve('res 1'),
+  3,
+  'a',
+  Promise.reject('rej 1'),
+  'b',
+  'c',
+  Promise.resolve('res 2'),
+];
+
+const promisedReduce = async (f, iterable, initV = null) => {
+  let acc;
+  let i = 0;
+
+  if (!iterable) {
+    try {
+      Symbol.iterator in initV;
+
+      iterable = initV;
+      acc = iterable.next().value;
+    } catch {
+      throw new Error('reduce function must be used with iterable');
+    }
+  } else {
+    acc = initV;
+  }
+
+  for (const v of iterable) {
+    acc = await f(acc, v, i++);
+  }
+
+  return acc;
+};
+
+const filter = (f, iterable) => {
+  let res = [];
+  for (const v of iterable) {
+    if (f(v) === true) res.push(v);
+  }
+  return res;
+};
+
+const filterAndReduce = (FF, RF, iterable, initV = null) => {
+  const toReduce = filter(FF, iterable);
+  return promisedReduce(RF, toReduce, initV);
+};
+
+filterAndReduce(
+  (x) => x instanceof Promise,
+  async (acc, cur, i) => {
+    acc.push([(await cur.catch(error)) ?? 'must have rejected', i]);
+    return acc;
+  },
+  target,
+  new Array()
+).then(log); // [ [ 'res 1', 0 ], [ 'must have rejected', 1 ], [ 'res 2', 2 ] ]
