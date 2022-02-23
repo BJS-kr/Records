8b32041a46ecc283a1034fa87b3ab8bb6f23a875
BJS-kr <78771384+BJS-kr@users.noreply.github.com>
2022-02-23 20:39:55 +0900

    Update go.js

--- functional_programming/go.js
+++ functional_programming/go.js
@@ -1,3 +1,7 @@
+import { filter } from './filter.js';
+import { map } from './map.js';
+import { items } from './resources.js';
+
 const log = console.log;
 
 const reduce = (f, initV = null, iterable) => {
@@ -24,12 +28,20 @@ const reduce = (f, initV = null, iterable) => {
   return acc;
 };
 
-const go = (...args) => reduce((acc, cur) => cur(acc), args);
+export const go = (...args) => reduce((acc, cur) => cur(acc), args);
 
 go(
   0,
   (x) => x + 1,
   (x) => x + 10,
   (x) => x + 100,
+  log
+);
+
+go(
+  items,
+  (items) => filter((item) => item.price >= 2000, items),
+  (filteredItems) => map((item) => item.price, filteredItems),
+  (prices) => reduce((acc, price) => acc + price, prices),
   log
 );
