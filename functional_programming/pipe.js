12dffe11344ce2182584050f3e2edd3622c2e3d1
BJS-kr <78771384+BJS-kr@users.noreply.github.com>
2022-02-23 20:40:21 +0900

    Update pipe.js

--- functional_programming/pipe.js
+++ functional_programming/pipe.js
@@ -1,18 +1,27 @@
 import { go } from './go.js';
+import { items } from './resources.js';
+import { filter } from './filter.js';
+import { map } from './map.js';
+import { reduce } from './reduce.js';
 
 const { log } = console;
 
-const pipe =
+export const pipe =
   (f, ...Fs) =>
   (...init) =>
     go(f(...init), ...Fs);
 
-log(
-  pipe(
-    (x, y, z) => x + y + z,
-    (x) => x + 1,
-    (x) => x + 10,
-    (x) => x + 100,
-    log
-  )(0, 1, 2)
-);
+pipe(
+  (x, y, z) => x + y + z,
+  (x) => x + 1,
+  (x) => x + 10,
+  (x) => x + 100,
+  log
+)(0, 1, 2);
+
+pipe(
+  (items) => filter((item) => item.price >= 2000, items),
+  (filteredItems) => map((item) => item.price, filteredItems),
+  (prices) => reduce((acc, price) => acc + price, prices),
+  log
+)(items);
