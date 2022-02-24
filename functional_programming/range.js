f12fa8c89c08270a7d089fe3506c26ced296e433
BJS-kr <78771384+BJS-kr@users.noreply.github.com>
2022-02-25 00:03:44 +0900

    Update range.js

--- functional_programming/range.js
+++ functional_programming/range.js
@@ -4,22 +4,25 @@ import { reduce } from './reduce.js';
 export const range = (range, step = 1, start = 0) => {
   if (step === 0) step = 1;
   if (range === start) return [];
-  if (range < 0 && step > 0) {
-    step = -1;
-  }
+  if (range < 0 && step > 0) step = -1;
+
   let i = start;
   let result = [];
+
   if (step < 0 && range < start) {
     while (i > range) {
       result.push(i);
       i += step;
     }
+
     return result;
   }
+
   while (i < range) {
     result.push(i);
     i += step;
   }
+
   return result;
 };
 
@@ -29,6 +32,8 @@ log(range(20, 2, 10)); // [10, 12, ..., 18]
 log(range(-5, -1, 5)); // [5, 4, ..., -4]
 log(range(10, 0, 10)); // []
 log(range(-10, 2, -1)); // step ignored and set to -1. so result is [-1, -2, ..., -9]
+log(range(-10, -2)); // [0, -2, ..., -8]
+log(range(-10)); // [0, -1, ..., -9]
 
 log(reduce((acc, cur) => acc + cur, range(10, 2, 4))); // 18
 
@@ -37,16 +42,17 @@ log(reduce((acc, cur) => acc + cur, range(10, 2, 4))); // 18
 export const lazyRange = function* (range, step = 1, start = 0) {
   if (step === 0) step = 1;
   if (range === start) return [];
-  if (range < 0 && step > 0) {
-    step = -1;
-  }
+  if (range < 0 && step > 0) step = -1;
+
   let i = start;
+
   if (step < 0 && range < start) {
     while (i > range) {
       yield i;
       i += step;
     }
   }
+
   while (i < range) {
     yield i;
     i += step;
