3c3c4987e98d42fc4c7e96f0e9b2de43b25fd702
BJS-kr <78771384+BJS-kr@users.noreply.github.com>
2022-02-25 21:27:55 +0900

    Update perf.js

--- functional_programming/perf.js
+++ functional_programming/perf.js
@@ -16,26 +16,45 @@ const obs = new O((l) => {
 
 obs.observe({ entryTypes: ['function', 'measure'] });
 
-P.mark('eval-all-start');
 trd((x, y, i) => {
   if (i > 5000) return x;
   return x + y;
 }, range(10000));
-P.mark('eval-all-end');
-P.measure('eval-all-result', 'eval-all-start', 'eval-all-end');
 
-P.mark('eval-lazy-start');
 trd((x, y, i) => {
   if (i > 5000) return x;
   return x + y;
 }, lazyRange(10000));
-P.mark('eval-lazy-end');
-P.measure('eval-lazy-result', 'eval-lazy-start', 'eval-lazy-end');
 
-// 결과: 압도적 차이
+try {
+  P.mark('eval-all-start');
+  reduce((x, y, i) => {
+    if (i > 500) throw new Error('this is intended');
+    return x + y;
+  }, range(1000000));
+} catch {
+  P.mark('eval-all-end');
+} finally {
+  P.measure('eval-all-result', 'eval-all-start', 'eval-all-end');
+}
+
+try {
+  P.mark('eval-lazy-start');
+  reduce((x, y, i) => {
+    if (i > 500) throw new Error('this is intended');
+    return x + y;
+  }, lazyRange(1000000));
+} catch {
+  P.mark('eval-lazy-end');
+} finally {
+  P.measure('eval-lazy-result', 'eval-lazy-start', 'eval-lazy-end');
+}
+
+
+// 결과: 압도적 차이. 평가해야하는 크기가 늘어날수록 이 차이는 더욱 심해진다.
 /**
- * eval-all-result 4.166100025177002
- * reduce 3.2450000047683716
- * eval-lazy-result 1.6902999877929688
- * reduce 1.6136999130249023
+ * reduce 3.241599917411804
+ * reduce 1.589900016784668
+ * eval-all-result 13.891700029373169
+ * eval-lazy-result 0.15079998970031738
  */
