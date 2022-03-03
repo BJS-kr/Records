702dbf6c51223acabee1da5ef73bb289fb3f19b4
BJS-kr <78771384+BJS-kr@users.noreply.github.com>
2022-03-03 23:33:07 +0900

    Update filter.js

--- functional_programming/filter.js
+++ functional_programming/filter.js
@@ -1,27 +1,22 @@
-const log = console.log;
+import { curry } from './curry.js';
+import { pipe } from './pipe.js';
+import { take } from './take.js';
+import { go1 } from './go.js';
+import { nop } from './resources.js';
 
-const m = new Map();
-
-m.set("a", 1);
-m.set("b", 2);
-
-const filter = (f, iterable) => {
-  let res = [];
-  for (const v of iterable) {
-    if (f(v) === true) res.push(v);
+export const lazyFilter = curry(function* (f, iter) {
+  iter = iter[Symbol.iterator]();
+  let cur;
+  while (!(cur = iter.next()).done) {
+    const curV = cur.value;
+    const curGo1 = go1(curV, f);
+    if (curGo1 instanceof Promise) {
+      // 여기서 promise상태로 yield해도 take가 promise를 풀어서 사용하기 때문에 상관없습니다.
+      yield curGo1.then((curGo1Res) =>
+        curGo1Res ? curV : Promise.reject(nop)
+      );
+    } else if (f(curV)) yield curV;
   }
-  return res;
-};
+});
 
-function* gen() {
-  for (let i = 0; i < 10; i++) {
-    yield i;
-  }
-}
-
-// return all odds
-log(filter((x) => x % 2 === 1, gen()));
-// eval multiple criteria
-log(filter(([k, v]) => k === "b" && v === 2, m));
-// this will return empty array
-log(filter(([k, v]) => k === "a" && v === 2, m));
+export const filter = curry(pipe(lazyFilter, take(Infinity)));
