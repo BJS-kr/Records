30f6978481c98c0c9ae77e5c4ff4b72f86b08f39
BJS-kr <78771384+BJS-kr@users.noreply.github.com>
2022-03-05 21:11:41 +0900

    Update reduce.js

--- functional_programming/reduce.js
+++ functional_programming/reduce.js
@@ -1,6 +1,22 @@
 import { curry } from './curry.js';
-import { go1 } from './go.js';
+import { lazyFilter } from './filter.js';
+import { go, go1 } from './go.js';
+import { lazyMap } from './map.js';
 import { nop } from './resources.js';
+
+const conReduce = curry((f, initV, iter) => {
+  // map등을 써서 iter를 catch가 달린 each element들로 변경하면 안됩니다.
+  // then과 다르게, catch는 오직 한 번밖에 사용할 수 없기 때문입니다.
+  // 이 부분에선 에러처리를 원하는 것이 아니라 unhandled rejection이 출력되는 것만 막고자 함인데,
+  // 아예 catch가 달린 element들로 배열을 변경해버린다면 이후의 작업에서 실제로 catch를 하고자 할때 처리가 되지 않습니다.
+  // 그러므로 iter를 catch가 달린 elem들로 재할당하지 않고, forEach를 통해 단지 동작이 없는 함수로 rejection error메세지만 출력하지 않도록 처리해두는 것입니다.
+  let conIter;
+  (conIter = iter ? [...iter] : [...initV]).forEach((each) =>
+    each instanceof Promise ? each.catch((e) => e) : each
+  );
+
+  return iter ? reduce(f, initV, conIter) : reduce(f, conIter);
+});
 
 const reduceF = (f, acc, curV, i) => {
   return curV instanceof Promise
@@ -8,7 +24,7 @@ const reduceF = (f, acc, curV, i) => {
         (curV) => f(acc, curV, i),
         (err) => (err == nop ? acc : Promise.reject(err))
       )
-    : f(acc, curV);
+    : f(acc, curV, i);
 };
 
 export const reduce = curry((f, initV = null, iterable) => {
@@ -32,7 +48,7 @@ export const reduce = curry((f, initV = null, iterable) => {
     let cur;
     while (!(cur = iterable.next()).done) {
       const curV = cur.value;
-      // acc가 변하기 때문에 똑같은 모양의 코드를 함수에 담아서 실행시켜야 한다는데 무슨 말인지 이해가 잘 안간다.
+      // 루프를 먼저 돌아버려 acc를 먼저 다시 대입하기 때문에 reduceF함수화
       acc = reduceF(f, acc, curV, i);
 
       if (acc instanceof Promise) {
@@ -45,3 +61,4 @@ export const reduce = curry((f, initV = null, iterable) => {
 
   return go1(acc, recur);
 });
+
