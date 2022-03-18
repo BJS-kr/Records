076ab523e36804748bbb39728184592bc3c99837
BJS-kr <hahajeng1234@gmail.com>
2022-03-19 02:06:07 +0900

    add description

--- algorithm/math/달팽이는 올라가고 싶다.js
+++ algorithm/math/달팽이는 올라가고 싶다.js
@@ -1,4 +1,5 @@
 // https://www.acmicpc.net/problem/2869
 const [a,b,v] = require('fs').readFileSync('/dev/stdin').toString().split(' ')
-
-console.log(Math.ceil((v-a) /(a-b) +1))
+// 올라가면 떨어지지 않으므로 마지막은 무조건 낮에 오르는 거리만큼 올라감
+// 즉 (총길이 - 올라가는 길이) / (올라가는 길이 - 떨어지는 길이) + 마지막 날(1)
+console.log(Math.ceil((v-a) /(a-b) + 1))
