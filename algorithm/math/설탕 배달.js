2ca7d29ceec352bc6eb0193cd7b4d37f9bf724cb
BJS-kr <hahajeng1234@gmail.com>
2022-03-19 20:39:02 +0900

    update 설탕배달

--- algorithm/math/설탕 배달.js
+++ algorithm/math/설탕 배달.js
@@ -1,13 +1,13 @@
 // https://www.acmicpc.net/problem/2839
-let kg = +require('fs').readFileSync('/dev/stdin').toString()
-let res = 0
-let proceed = true;
-while ((kg % 5) % 3 !== 0) {
-  if (kg < 3) {console.log(-1); proceed = false; break;}
-  kg -= 3
-  res++
-}
-if (proceed) {
-  res += Math.floor(kg/5) + (kg % 5) / 3
-  console.log(res)
+const kg = +require('fs').readFileSync('/dev/stdin').toString()
+
+if (kg % 5 === 0) console.log(kg / 5);
+else {
+  let maxFive = Math.floor(kg / 5)
+  let possible = false;
+  while (maxFive >= 0) {
+    if((kg - maxFive * 5) % 3 === 0) {possible = true; break};
+    maxFive--
+  }
+  console.log(!possible ? -1 : maxFive + (kg - maxFive * 5) / 3)
 }
