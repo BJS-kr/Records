22c86789177074b7f79fb3b300bb1aa499609a5a
BJS-kr <hahajeng1234@gmail.com>
2022-03-30 21:17:50 +0900

    update 택시기하학

--- algorithm/math/택시 기하학.js
+++ algorithm/math/택시 기하학.js
@@ -1,3 +1,3 @@
-const r = +require('fs').readFileSync('./input.txt').toString()
+const r = +require('fs').readFileSync('/dev/stdin').toString()
 console.log((r**2*3.141592653589793238462643).toFixed(6))
 console.log(((Math.sqrt(2)*r)**2).toFixed(6))
