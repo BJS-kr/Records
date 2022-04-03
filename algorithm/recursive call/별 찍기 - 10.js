7fa7a5d0c2f17ae54834392e5517a45f28ed636d
BJS-kr <hahajeng1234@gmail.com>
2022-04-04 00:01:57 +0900

    update 별찍기

--- algorithm/recursive call/별 찍기 - 10.js
+++ algorithm/recursive call/별 찍기 - 10.js
@@ -11,8 +11,8 @@
 // 규칙을 사용하여, 구현 순서는 다음과 같이 정의할 수 있다.
 // 1. 입력 받은 N만큼, N*N 2차원 배열을 생성한다.
 // 2. N/3에 도달할때까지, 1부터 3씩 곱해지는 수열을 생성한다.
-// 3. 생성된 수열에 대하여, 순차적으로 규칙4가 적용되는 line의 index로 이루어진 수열을 생성한다.
-// 4. index의 수열에 대하여, 순차적으로 규칙5를 적용한다.
+// 3. 생성된 수열에 대하여, 순차적으로 규칙1이 적용되는 line의 index로 이루어진 수열을 생성한다.
+// 4. index의 수열에 대하여, 순차적으로 규칙2를 적용한다.
 
 // ***************************
 // * ** ** ** ** ** ** ** ** *
@@ -41,3 +41,50 @@
 // ***************************
 // * ** ** ** ** ** ** ** ** *
 // ***************************
+
+// 구현 1: 이중 배열 생성
+const N = +require('fs').readFileSync('/dev/stdin').toString();
+let target = [];
+let count = 1;
+while (count++ <= N) {
+  target.push(Array(N).fill('*'));
+}
+
+// 구현 2: 필요한 공백의 수열 생성
+const spaces = [];
+let space = 1;
+while (space <= N / 3) {
+  spaces.push(space);
+  space *= 3;
+}
+
+for (const space of spaces) {
+  // 구현 3: 수열에 대하여 규칙 1 적용
+  const spaceWillBeAffectTo = [];
+  let startPoint = 0;
+  while (startPoint <= N - space) {
+    let row = startPoint + space;
+
+    while (row < startPoint + space * 2) {
+      spaceWillBeAffectTo.push(row++);
+    }
+    startPoint += 3 * space;
+  }
+
+  // 구현 4: 영향을 받는 각 라인에 대하여, space만큼의 공백에 규칙 2 적용
+  for (const index of spaceWillBeAffectTo) {
+    let startPoint = 0;
+    while (startPoint <= N - space) {
+      let spaceIndex = startPoint + space;
+      const spaceEnd = spaceIndex + space;
+      while (spaceIndex < spaceEnd) {
+        target[index][spaceIndex++] = ' ';
+      }
+      startPoint += 3 * space;
+    }
+  }
+}
+
+for (const v of target) {
+  console.log(v.join(''));
+}
