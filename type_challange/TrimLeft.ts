270a37b5f01e03fcf94b9278bbee9ed4bbad4304
BJS-kr <hahajeng1234@gmail.com>
2022-04-25 03:18:54 +0900

    add TrimLeft

---
+++ type_challange/TrimLeft.ts
@@ -0,0 +1,8 @@
+/**
+ * Question
+ * Implement TrimLeft<T> which takes an exact string type and returns a new string with the whitespace beginning removed.
+ */
+
+// Answer
+type TrimLeft<S extends string> = S extends ` ${infer R}` ? TrimLeft<R> : S;
+type trimed = TrimLeft<'  Hello World  '>; // expected to be 'Hello World  '
