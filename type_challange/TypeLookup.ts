2f6eacb424066d48b0b69775133c3e7457969da3
BJS-kr <hahajeng1234@gmail.com>
2022-04-25 03:02:38 +0900

    add TypeLookup

---
+++ type_challange/TypeLookup.ts
@@ -0,0 +1,27 @@
+/**
+ * Question
+ * Sometimes, you may want to lookup for a type in a union to by their attributes.
+ * In this challenge, we would like to get the corresponding type by searching for the common type field in the union Cat | Dog.
+ * In other words, we will expect to get Dog for LookUp<Dog | Cat, 'dog'> and Cat for LookUp<Dog | Cat, 'cat'> in the following example.
+ */
+
+// Answer
+
+type LookUp<T, Q extends string> = T extends { type: string }
+  ? Q extends T['type']
+    ? T
+    : never
+  : never;
+
+interface Cat {
+  type: 'cat';
+  breeds: 'Abyssinian' | 'Shorthair' | 'Curl' | 'Bengal';
+}
+
+interface Dog {
+  type: 'dog';
+  breeds: 'Hound' | 'Brittany' | 'Bulldog' | 'Boxer';
+  color: 'brown' | 'white' | 'black';
+}
+
+type MyDogType = LookUp<Cat | Dog, 'dog'>; // expected to be `Dog`
