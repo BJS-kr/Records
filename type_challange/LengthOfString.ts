da04ed4dc4d097e15ec6f0399bd6cffc20e69b7b
BJS-kr <hahajeng1234@gmail.com>
2022-04-29 02:18:12 +0900

    update LengthOfString

--- type_challange/LengthOfString.ts
+++ type_challange/LengthOfString.ts
@@ -4,8 +4,10 @@
  */
 
 // Answer
-
-type StringLength<S extends string> = S extends `${infer Start}${infer End}`
-  ? [Start, StringLength<End>]
+type StringLength<S extends string> = S extends ''
+  ? []
+  : S extends `${infer Start}${infer End}`
+  ? [Start, ...StringLength<End>]
   : never;
-type StringLengthMeasured = StringLength<'hello'>;
+
+type StringLengthMeasured = StringLength<'helloBye'>['length'];
