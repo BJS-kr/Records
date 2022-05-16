492ffb0884241bd03b367d6098804065beb932e5
BJS-kr <hahajeng1234@gmail.com>
2022-05-16 23:22:46 +0900

    update select

--- SQL/HackerRank.SELECT.basics.sql
+++ SQL/HackerRank.SELECT.basics.sql
@@ -83,3 +83,16 @@ SELECT name FROM Employee ORDER BY name;
 
 -- Employee Salaries
 SELECT name FROM Employee WHERE salary > 2000 AND months < 10;
+
+-- the blunder
+-- https://www.hackerrank.com/challenges/the-blunder/problem?isFullScreen=true&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+-- 핵심은 형 변환 및 문자, 숫자 함수 활용
+SELECT \
+  CEIL(\
+    (SELECT AVG(ALL Salary) FROM EMPLOYEES) - \
+    (SELECT AVG(ALL 
+    CONVERT(
+      REPLACE(
+        CONVERT(Salary, CHAR), '0', ''), SIGNED)) 
+    FROM EMPLOYEES)
+    );
--- SQL/TheBlunder.sql
+++
@@ -1,7 +0,0 @@
--- https://www.hackerrank.com/challenges/the-blunder/problem?isFullScreen=true&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
--- 핵심은 형 변환 및 문자, 숫자 함수 활용
-SELECT \
-  CEIL(\
-    (SELECT AVG(ALL Salary) FROM EMPLOYEES) - \
-    (SELECT AVG(ALL CONVERT(REPLACE(CONVERT(Salary, CHAR), '0', ''), SIGNED)) FROM EMPLOYEES)
-    );
