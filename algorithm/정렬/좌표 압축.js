086fb9c6fba21c42ada64e16d6b8653ba317a66e
BJS-kr <hahajeng1234@gmail.com>
2022-05-22 00:41:13 +0900

    update 좌표 압축

--- algorithm/정렬/좌표 압축.js
+++ algorithm/정렬/좌표 압축.js
@@ -5,6 +5,7 @@ function go() {
     .trim()
     .split('\n');
 
+  this.result = '';
   this.cds = coordinates[0].split(' ').map((x) => +x);
   this.duplicateRemoved = [...new Set(cds)].sort((a, b) => a - b);
   this.greaterDictionary = duplicateRemoved.reduce(
@@ -12,7 +13,8 @@ function go() {
     {}
   );
 
-  this.result = '';
+  // 메모리 관리하고 싶어서 넣어봤습니다 ㅎㅎ 테스트해보니 별 차이는 없네요
+  delete this.duplicateRemoved;
 
   for (const cd of this.cds) {
     this.result += `${this.greaterDictionary[cd]} `;
