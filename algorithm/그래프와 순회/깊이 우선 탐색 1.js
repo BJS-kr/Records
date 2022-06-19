d248111d9118724056a97689f6baefbdaaa40a58
BJS-kr <hahajeng1234@gmail.com>
2022-06-19 15:13:57 +0900

    update

--- algorithm/그래프와 순회/깊이 우선 탐색 1.js
+++ algorithm/그래프와 순회/깊이 우선 탐색 1.js
@@ -1,5 +1,5 @@
 const [_, ...nodes] = require('fs')
-  .readFileSync('../input.txt')
+  .readFileSync(process.platform === 'linux' ? '/dev/stdin' : '../input.txt')
   .toString()
   .split('\n')
   .map((x) => x.split(' ').map(Number));
@@ -13,11 +13,33 @@ const graph = nodes.reduce(
   new Map()
 );
 
-const visit = ((n, m) => {
+graph.forEach((arr) => arr.sort((x, y) => x - y));
+
+const [visit, result] = ((n, m, r) => {
   for (let i = 1; i <= n; i++) {
     m.set(i, false);
+    r.set(i, 0);
   }
-  return m;
-})(_[0], new Map());
+  return [m, r];
+})(_[0], new Map(), new Map());
 
-function dfs() {}
+visit.set(_[2], true);
+result.set(_[2], 1);
+let num = 2;
+function dfs(cur) {
+  for (const n of graph.get(cur)) {
+    !visit.get(n) && (visit.set(n, true), result.set(n, num++), dfs(n));
+  }
+}
+
+dfs(_[2], 2);
+let resultStr = '';
+for (let i = 1; i <= _[0]; i++) {
+  resultStr += result.get(i) + '\n';
+}
+
+console.log(resultStr);
+
+// 이거 js로 풀면 채점하다가 type error가 나온다.
+// 뭐지 싶어서 구글링해보니 다른 사람도 나와 같은 증상을 겪고 있다.
+// 내가 무조건 맞았다는 것이 아니라 테스트 케이스들이 잘 나오고 기본적인 구현에서 채점이 진행되다가 타입에러가 나는 상황이 이해가 안된다.
